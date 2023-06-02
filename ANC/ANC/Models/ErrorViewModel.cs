9e89daa3bbedb42cb2ffea169a694d646dc69b9b
maicol hernandez <maykolandres4@gmail.com>
2023-06-02 03:58:09 -0500

    CONNECTED

---
+++ ANC/ANC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ANC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
