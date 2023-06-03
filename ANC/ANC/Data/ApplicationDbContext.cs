d536a5855edfc6328225365b9f839f2cf0bfbfc3
maicol hernandez <maykolandres4@gmail.com>
2023-06-03 01:19:58 -0500

    conntected to connected string & installed sql server

--- ANC/ANC/Data/ApplicationDbContext.cs
+++ ANC/ANC/Data/ApplicationDbContext.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ANC.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace ANC.Data
+namespace ANC.Data;
+
+public class ApplicationDbContext : DbContext
 {
-    public class ApplicationDbContext : DbContext
-    {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)    
+    {    
     }
+
+    //connection to DB and create table
+    public DbSet<Category> Categories { get; set; }
 }
--- ANC/ANC/Program.cs
+++ ANC/ANC/Program.cs
@@ -1,7 +1,15 @@
+
+
+using ANC.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
