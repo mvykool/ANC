1b149072a5b128b8338bfa1762f296fbaaeecaf6
maicol hernandez <maykolandres4@gmail.com>
2023-06-03 03:35:11 -0500

    added bootstrap

--- ANC/ANC/Controllers/CategoryController.cs
+++ ANC/ANC/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ANC.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ANC.Controllers
 {
     public class CategoryController : Controller
     {
+        //create db
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
